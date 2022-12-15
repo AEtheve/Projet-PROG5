eaebdc592c38b27b5d4d5fc110154282f507e9bb
ar0x18 <79864262+ar0x18@users.noreply.github.com>
2022-12-15 14:31:22 +0100

    Update affichage_entete.c

--- affichage_entete.c
+++ affichage_entete.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 void printMagic(unsigned char *entete){
-    printf("Magic: ");
+    printf(" Magic:   ");
     for (int i = 0; i < 16; i++){
         printf("%02x ", entete[i]);
     }
@@ -12,7 +13,7 @@ void printMagic(unsigned char *entete){
 
 
 void printClass(unsigned char *entete){
-    printf("Class: ");
+    printf("  Class:                             ");
     if (entete[4] == 1){
         printf("ELF32\n");
     } else if (entete[4] == 2){
@@ -23,7 +24,7 @@ void printClass(unsigned char *entete){
 }
 
 void printData(unsigned char *entete){
-    printf("Data: ");
+    printf("  Data:                              ");
     if (entete[5] == 1){
         printf("LSB\n");
     } else if (entete[5] == 2){
@@ -34,7 +35,7 @@ void printData(unsigned char *entete){
 }
 
 void printVersion(unsigned char *entete){
-    printf("Version: ");
+    printf("  Version:                           ");
     if (entete[6] == 1){
         printf("1 (current)\n");
     } else {
@@ -43,7 +44,7 @@ void printVersion(unsigned char *entete){
 }
 
 void printAbi(int val){
-    printf("ABI: ");
+    printf("  OS/ABI:                            ");
     switch (val){
         case 0:
             printf("UNIX System V\n");
@@ -91,11 +92,11 @@ void printAbi(int val){
 }
 
 void printAbiVersion(unsigned char *entete){
-    printf("ABI Version: %d\n", entete[8]);
+    printf("  ABI Version:                       %d\n", entete[8]);
 }
 
 void printType(unsigned char *entete){
-    printf("Type: ");
+    printf("  Type:                              ");
     if (entete[16] == 1){
         printf("Relocatable file\n");
     } else if (entete[16] == 2){
@@ -111,7 +112,7 @@ void printType(unsigned char *entete){
 
 
 void printMachine(unsigned char *entete){
-    printf("Machine: ");
+    printf("  Machine:                           ");
     if (entete[18] == 1){
         printf("AT&T WE 32100\n");
     } else if (entete[18] == 2){
@@ -134,6 +135,64 @@ void printMachine(unsigned char *entete){
     }
 }
 
+void printVersion2(unsigned char *entete){
+    printf("  Version:                           ");
+    printf("0x%x\n",entete[20]); // TODO  escrot #1 je ne sais pas ce que fait entete[19]
+}
+
+void printPointDentree(unsigned char *entete) {
+    printf("  Entry point address:               ");
+    printf("0x%x\n",entete[21]); // TODO  escrot #2 peut être pas 21
+}
+
+void printStartOfProgramHeaders (unsigned char *entete) {
+    printf("  Start of program headers:          ");
+    printf("0 (bytes into file)\n"); //TODO start of program à faire
+}
+
+void printStartOfSectionHeaders (unsigned char *entete) {
+    printf("  Start of section headers:          ");
+    uint32_t v =entete[33] << 8;
+    v = v | entete[32]; 
+    printf("%d (bytes into file)\n",v);
+}
+
+void printFlags (unsigned char *entete){
+    printf("  Flags:                             ");
+    printf("0x5000000, Version5 EABI\n"); //TODO Flags à faire
+}
+
+void printsizeOfHeaders (unsigned char *entete)  {
+    printf("  Size of this header:               ");
+    printf("52 (bytes)\n"); //TODO sizeOfHeaders à faire
+}
+
+void printsizeOfProgramHeaders (unsigned char *entete)  {
+    printf("  Size of program headers:           ");
+    printf("0 (bytes)\n"); //TODO sizeOfProgramHeaders à faire
+}
+
+void printNumberOfProgramHeaders (unsigned char *entete)  {
+    printf("  Number of program headers:         ");
+    printf("0\n"); //TODO NumberOfProgramHeaders à faire
+}
+
+void printSizeOfSectionHeaders (unsigned char *entete)  {
+    printf("  Size of section headers:           ");
+    printf("40 (bytes)\n"); //TODO SizeOfSectionHeaders à faire
+}
+
+void printNumberOfSectionHeaders (unsigned char *entete)  {
+    printf("  Number of section headers:         ");
+    printf("21\n"); //TODO NumberOfSectionHeaders à faire
+}
+
+void printSectionHeaderStringTableIndex (unsigned char *entete)  {
+    printf("  Section header string table index: ");
+    printf("20\n"); //TODO SectionHeaderStringTableIndex à faire
+}
+
+
 int main(int argc, char *argv[]){
     if (argc != 2){
         printf("Usage: %s <fichier>\n", argv[0]);
@@ -149,10 +208,10 @@ int main(int argc, char *argv[]){
     }
 
 
-    unsigned char entete[32];
+    unsigned char entete[50];
 
     fread(entete, 1, 32, f_bin);
-
+    printf(" ELF Header:\n ");
     printMagic(entete);
 
     printClass(entete);
@@ -163,7 +222,17 @@ int main(int argc, char *argv[]){
 
     printType(entete);
     printMachine(entete);
-    printVersion(entete);
-    
+    printVersion2(entete);
+    printPointDentree(entete);
+    printStartOfProgramHeaders(entete);
+    printStartOfSectionHeaders (entete);
+    printFlags(entete);
+    printsizeOfHeaders(entete);
+    printsizeOfProgramHeaders(entete);
+    printNumberOfProgramHeaders(entete);
+    printSizeOfSectionHeaders(entete);
+    printNumberOfSectionHeaders(entete);
+    printSectionHeaderStringTableIndex(entete);
+
     return 0;
 }
