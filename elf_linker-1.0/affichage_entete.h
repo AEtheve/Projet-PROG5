1119fe0fb817997881a377412580cc87d7d712ed
LeVraiCrab <clement.raspail01@orange.fr>
2022-12-16 16:30:36 +0100

    Lien entete section

--- elf_linker-1.0/affichage_entete.h
+++ elf_linker-1.0/affichage_entete.h
@@ -1,1 +1,11 @@
+
+typedef struct{
+    int start_section;
+    int nb_sections;
+    int taille_section_header;
+    int section_header_string_table_index;
+}ElfHeader;
+
+ElfHeader* valeur_entete(char *nom_fichier);
+
 void affichage_entete(char *nom_fichier);
