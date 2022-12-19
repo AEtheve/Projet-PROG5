3d0df6a5e6a85a36debf3f6cf55fb69c97706cf5
AEtheve <30652308+AEtheve@users.noreply.github.com>
2022-12-19 14:40:57 +0100

    Modification des tests d'affichage section (big endian)

--- elf_linker-1.0/tests/test_affichage_entete.c
+++ elf_linker-1.0/tests/test_affichage_entete.c
@@ -5,15 +5,23 @@
 #include "./utils.h"
 #include "../affichage_entete.h"
 
-void TestAffichageEntete(CuTest *tc)
+void TestAffichageEntete1(CuTest *tc)
 {    
     writeStdout();
     affichage_entete("./tests/file1.o");
     RunTest("./tests/file1.expected", tc);
+    clearStdout();
+}
+
+void TestAffichageEntete2(CuTest *tc)
+{
+    writeStdout();
     affichage_entete("./tests/file2.o");
     RunTest("./tests/file2.expected", tc);
     clearStdout();
 }
+
+
 
 int main(void)
 {
@@ -21,7 +29,8 @@ int main(void)
     CuString *output = CuStringNew();
     CuSuite* suite = CuSuiteNew();
     
-    SUITE_ADD_TEST(suite, TestAffichageEntete);
+    SUITE_ADD_TEST(suite, TestAffichageEntete1);
+    SUITE_ADD_TEST(suite, TestAffichageEntete2);
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
