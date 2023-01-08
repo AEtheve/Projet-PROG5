5f98d19ec58e6a4aabf4aa82217842b7ed33535c
ThilenSavignon <thilen.savignon@gmail.com>
2023-01-08 17:28:49 +0100

    Creation module fusion_relocation

---
+++ elf_linker-1.0/fusion_relocation.c
@@ -0,0 +1,10 @@
+#include "fusion_relocation.h"
+
+Elf* fusionRelocation(Elf* result, Elf* elf1, Elf* elf2) {
+
+    
+
+
+
+    return result;
+}
---
+++ elf_linker-1.0/fusion_relocation.h
@@ -0,0 +1,12 @@
+#ifndef _FUSION_RELOCATION_H_
+#define _FUSION_RELOCATION_H_
+
+#include "util.h"
+#include "elf32.h"
+
+Elf* fusionRelocation(Elf* result, Elf* elf1, Elf* elf2);
+
+
+
+
+#endif
