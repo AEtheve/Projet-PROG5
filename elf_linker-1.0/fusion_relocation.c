0b63da63a521362cc0d386a6d31656f43f75c496
LeVraiCrab <clement.raspail01@orange.fr>
2023-01-10 15:50:58 +0100

    Nettoyage du code plus version (final ?) de relocation

--- elf_linker-1.0/fusion_relocation.c
+++ elf_linker-1.0/fusion_relocation.c
@@ -44,6 +44,38 @@ int whereIsBryansSection(Elf *elf, char *name){
     return -1;
 }
 
+int getIndexSymVal(Elf* res, Elf* elf, int index){
+    char* name;
+    if((elf->symbol_header + index)->info == 3){
+        name = getNameSection(elf->symbol_header,elf->section_header,index);
+    } else {
+        name = getName(elf->symbol_header,elf->string_header,index);
+    }
+
+    for(int i = 0; i <= res->nb_symbol; i++){
+        if((res->symbol_header + i)->info == 3){
+            if (!strcmp(name,getNameSection(res->symbol_header,res->section_header,i))){
+                return i;
+            }
+        } else {
+            if (!strcmp(name,getName(res->symbol_header,res->string_header,i))){
+                return i;
+            }
+        }
+    }
+    printf("Erreur, le symbole n'existe pas\n");
+    exit(1);
+}
+
+int getSectionSize(Elf *elf,char *name){
+    for (int i=0;i<elf->header->e_section_header_entry_count; i++){
+      if (!strcmp(name,elf->section_header[i].name)){
+        return i;
+      }
+    }
+    return 0;
+}
+
 Elf* fusionRelocation(Elf* result, Elf* elf1, Elf* elf2) {
 
     result->relocation_header = malloc(elf1->nb_reloc*sizeof(ElfRelocation));
@@ -57,7 +89,8 @@ Elf* fusionRelocation(Elf* result, Elf* elf1, Elf* elf2) {
         if (elf2->section_header[i].entree.type == 9){
             ElfSection* section2 = &elf2->section_header[i];
             testElf1 = isTheSectionInSecondTable(elf1, section2->name); 
-            if (testElf1 == -1) { // Si Section de ELF2 n'est pas dans ELF1
+            if (testElf1 == -1) { 
+                /* Ajout de la section de ELF2 dans ELF1 */
                 ElfRelocation* tmp;
                 tmp = realloc(result->relocation_header, (result->nb_reloc+1)*sizeof(ElfRelocation));
                 if (tmp == NULL){
@@ -70,19 +103,18 @@ Elf* fusionRelocation(Elf* result, Elf* elf1, Elf* elf2) {
                 section2->entree.link = findSymTab(result);
                 section2->entree.info = positionSectionWithoutRel(result, section2->name);
                 addSection(result, *section2);
-            } else { // Fusion de ELF1 et ELF2tmp 
-
+            } else { 
+                /* Fusion des sections de ELF1 et ELF2 */
                 int size1 = elf1->section_header[testElf1].entree.size;
                 int size2 = section2->entree.size;
-
                 elf1->section_header[testElf1].entree.size = size1 + size2;
+
 
                 int index_relocation_elf1 = whereIsBryansSection(elf1, section2->name);
 
                 RelocationHeader* tmp;
-                tmp = (RelocationHeader*)malloc((size1 + size2)*sizeof(RelocationHeader));
+                tmp = (RelocationHeader*)malloc((size1 + size2));
 
-                //realloc(result->relocation_header[index_relocation_elf1].entree, (size1 + size2));
                 if (tmp == NULL){
                     printf("Erreur\n");
                     exit(1);
@@ -90,23 +122,25 @@ Elf* fusionRelocation(Elf* result, Elf* elf1, Elf* elf2) {
                 
                 memcpy(tmp, elf1->relocation_header[index_relocation_elf1].entree, size1);
 
-                printf("TEST : %x\n",elf2->relocation_header[compt].entree->offset);
-                memcpy(tmp + size1, elf2->relocation_header[compt].entree, size2); 
-
-                
-                // printf("ICI : %d\n",size1 / elf1->section_header[testElf1].entree.entsize);
-
 
                 int cmp_tmp = 0;
-                for(int j = size1; j < (size1 + size2) ; j += 8){
-                    // (tmp + size1) = elf2->relocation_header[compt].entree;
-                    (tmp + j)->offset += size1;
-                    printf("COMPT : %x\n",(tmp + j)->offset );
+                int ndx;
+                for(int j = size1/8 ; j < (size1 + size2)/8 ; j ++){
+                    tmp[j] = elf2->relocation_header[compt].entree[cmp_tmp];
+
+
+                    ndx=getIndexSymVal(result, elf2, tmp[j].info>>8);
+                    tmp[j].info = (tmp[j].info & 0xFF) | (ndx << 8);
+
+
+                    int val = getSectionSize(elf1, getNameSection(result->symbol_header,result->section_header,ndx));
+                    tmp[j].offset += val;
+                    
+
                     cmp_tmp++;
                 }
 
                 result->relocation_header[index_relocation_elf1].entree = tmp;
-                // printf("FINAL : %x\n",result->relocation_header[size1/8].entree->offset);
                 elf1->section_header[testElf1].entree.link = findSymTab(result);
                 elf1->section_header[testElf1].entree.info = positionSectionWithoutRel(result, elf1->section_header[testElf1].name);
                 addSection(result, elf1->section_header[testElf1]);
@@ -114,48 +148,28 @@ Elf* fusionRelocation(Elf* result, Elf* elf1, Elf* elf2) {
             compt++;
         }
     }
-
-
-
     return result;
 }
 
-int main (int argc, char** argv){
-    FILE* f1 = ouvertureFichier(argv[1], "rb");
+void fonctionTestRelocation (char* nom_fichier1, char* nom_fichier2){
+    FILE* f1 = ouvertureFichier(nom_fichier1, "rb");
     Elf* elf1 = valeurEntete(f1);
     elf1 = valeurSection(elf1, f1);
     elf1 = getTableSymboles(elf1, f1);
     elf1 = getTableRelocation(elf1, f1);
 
-    printf("Partie 1 : \n");
-    affichageTableReimplentation(elf1);
-
-    FILE* f2 = ouvertureFichier(argv[2], "rb");
+    FILE* f2 = ouvertureFichier(nom_fichier2, "rb");
     Elf* elf2 = valeurEntete(f2);
     elf2 = valeurSection(elf2, f2);
     elf2 = getTableSymboles(elf2, f2);
     elf2 = getTableRelocation(elf2, f2);
 
-    printf("\n\n\nPartie 2 : \n");
-    affichageTableReimplentation(elf2);
-
     Elf* result;
-    printf("\n\n\nPartie res : \n");
     result = fusionSection(elf1, elf2);
-    // affichageSection(result,false);
-    printf("\n\nFusion section OK\n");
-
     result = fusion_table_symboles(elf1, elf2, result);
-    printf("\n\nFusion table symbole OK\n");
-    affichageSection(result,false);
-    // affichageTableSymbole(result);
 
     result = fusionRelocation(result, elf1, elf2);
-    printf("\n\nFusion relocation OK\n");
     
-    // affichageSection(elf1,false);
-    affichageSection(result,false);
-    // affichageTableSymbole(result);
     affichageTableReimplentation(result);
 
     freeElf(elf1);
@@ -163,5 +177,4 @@ int main (int argc, char** argv){
     free(result);
     fermetureFichier(f1);
     fermetureFichier(f2);
-    return 0;
 }
